7d6b1895a6007bc12f164b5eee0b6c540c1181e3
ahmedfadl156 <af38765222@gmail.com>
2026-04-23 04:33:52 +0200

    update next.config.ts to use beforeFiles for API rewrites

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   async rewrites() {
-    return [
-      {
-        source: "/api/v1/:path*",
-        destination: `https://cinefantastybackend-production-eb3a.up.railway.app/api/v1/:path*`
-      }
-    ]
+    return {
+      beforeFiles: [
+        {
+          source: "/api/v1/:path*",
+          destination: `https://cinefantastybackend-production-eb3a.up.railway.app/api/v1/:path*`
+        }
+      ]
+    };
   },
   images: {
     remotePatterns: [
