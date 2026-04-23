082adc25388c273671e4e7de2e1da0c344e2b240
ahmedfadl156 <af38765222@gmail.com>
2026-04-23 03:54:48 +0200

    add API rewrites to proxy requests to the production backend server

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/v1/:path*",
+        destination: `https://cinefantastybackend-production-eb3a.up.railway.app/api/v1/:path*`
+      }
+    ]
+  },
   images: {
     remotePatterns: [
       {
